9e6a7c7e889f687902715b6754dcc19dff5a89ea
Nexints <163356377+Nexints@users.noreply.github.com>
2024-09-02 02:31:27 -0400

    Patch #11

--- js/copy.js
+++ js/copy.js
@@ -2,10 +2,10 @@ function copyTheText() {
   let Text = document.getElementById('copyable').innerHTML;
 
   // Copy the text inside the text field
-  navigator.clipboard.writeText(Text.value);
+  navigator.clipboard.writeText(Text);
   
   let tooltip = document.getElementById("copyToClipboard");
-  tooltip.innerHTML = "Copied: " + Text.value;
+  tooltip.innerHTML = "Copied: " + Text;
 }
 function resetCopy() {
   let tooltip = document.getElementById("copyToClipboard");
