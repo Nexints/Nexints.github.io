75a13a2735d13b637255efa8bfdba04b65b80fb0
Nexints <163356377+Nexints@users.noreply.github.com>
2024-09-01 23:51:03 -0400

    Bugfixing #1

--- js/copy.js
+++ js/copy.js
@@ -1,9 +1,5 @@
-function copyTheText(copyText) {
+function copyTheText() {
   var copyText = document.getElementById("myInput".innerHTML);
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
 
   // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.value);
