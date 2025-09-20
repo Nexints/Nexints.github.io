51361abb434da7577a75bbc4609f9b9be0fb639f
Nexints <163356377+Nexints@users.noreply.github.com>
2025-09-20 19:35:20 -0400

    heh
    Website recode in NextJS!

---
+++ next.config.mjs
@@ -0,0 +1,40 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                fs: false,
+            };
+        }
+
+        return config;
+    },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    },
+    async redirects() {
+        return [
+            // Basic redirect
+            {
+                source: '/discord',
+                destination: 'https://discord.gg/invite/Ht7B5Urr9y',
+                permanent: true,
+            },
+        ]
+    },
+}
+
+export default nextConfig;
